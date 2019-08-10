46dab83abc0774ea66da14e519ce62222dec3551
JP Horrigan <Horrigan.JP@outlook.com>
2019-08-10 15:15:21 +0000

    Lesson 9 complete

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -22,6 +22,17 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#create action" do
+    before do
+      sign_in @user do
+        it "should properly deal with validation errors" do
+          post :create,  params: { gram: { message: '' } }
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(Gram.count).to eq 0
+        end
+      end
+    end
+  end
   #describe "grams#new" do
   #  context "as an authenticiated user" do
   #    it "should successfuly show new form page" do
@@ -32,18 +43,24 @@ RSpec.describe GramsController, type: :controller do
   #  end
   #end
 
-  # describe "grams#create action" do
-  #   before do
-  #     sign_in @user do
-  #       it "should successfully post gram entry to database" do
-  #         post :create, params: { gram: { message: 'Hello!'} }
-  #         expect(response).to redirect_to root_path
+  describe "grams#create action" do
+    before do
+      sign_in @user do
+        it "should successfully post gram entry to database" do
+          post :create, params: { gram: { message: 'Hello!'} }
+          expect(response).to redirect_to root_path
 
-  #         gram = Gram.last
-  #         expect(gram.message).to eq("Hello!")
-  #       end
-  #     end
-  #   end
-  # end
+          gram = Gram.last
+          expect(gram.message).to eq("Hello!")
+        end
+
+        # it "should properly deal with validation errors" do
+        #   post :create,  params: { gram: { message: '' } }
+        #   expect(response).to have_http_status(:unprocessable_entity)
+        #   expect(Gram.count).to eq 0
+        # end
+      end
+    end
+  end
 end
 
