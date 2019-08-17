53486163e2c225f5bd64cbe13e04c6fa528e3e1d
JP Horrigan <Horrigan.JP@outlook.com>
2019-08-17 16:28:11 +0000

    Lesson 18 Complete

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def new
+    @comment = Comment.new
+  end 
+
+  def create
+    @gram = Gram.find_by_id(params[:gram_id])
+    return render_not_found if @gram.blank?
+    @gram.comments.create(comment_params.merge(user: current_user))
+    redirect_to root_path
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:message)
+    end
+end
